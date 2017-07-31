da55d3f9a3543dfb8f3c3495dd733fb1c7cfba09
NorbertNader <norbert.nader@gmail.com>
2017-07-31 15:55:58 +0200

    [add] api routes

--- server.js
+++ server.js
@@ -52,6 +52,28 @@ app.get('/setup', function(req, res) {
 });
 
 // API ROUTES -------------------
+// get an instance of the router for api routes
+var apiRoutes = express.Router(); 
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});   
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
 // we'll get to these in a second
 
 // =======================
