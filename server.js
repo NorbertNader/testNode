5d934f2c6f3ec1665198b1bf69978c11c9ef5614
NorbertNader <norbert.nader@gmail.com>
2017-07-31 15:51:18 +0200

    [add] setup route

--- server.js
+++ server.js
@@ -33,6 +33,24 @@ app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
+app.get('/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new User({ 
+    name: 'Some person', 
+    password: 'password',
+    admin: true 
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
 // API ROUTES -------------------
 // we'll get to these in a second
 
