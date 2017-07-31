39059900a870c9d7e58ad9f431017efe63f7f0e9
NorbertNader <norbert.nader@gmail.com>
2017-07-31 15:42:31 +0200

    [fix] config to db uri

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'secret': 'oooohyeahsuchawesomnesss',
-    'database': 'mongodb://admin:admin&54;localhost:27017/mydb'
+    'database': 'mongodb://admin:admin@localhost:27017/mydb'
 
 };
